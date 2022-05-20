d668900c973505484810ed90a00fabad3106b3a2
Vladimir Below <laitvladimir@yandex.ru>
2022-05-20 17:17:29 +0300

    New: Анимировал меню

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
 
 import { Menu } from './components/menu'
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import { motion, AnimatePresence } from 'framer-motion'
 
 import githubIcon from '../icon_sox/github.png'
 import vkIcon from '../icon_sox/vk-logo.png'
@@ -8,21 +9,32 @@ import menu_open from '../icon_sox/menu_open.svg'
 import menu_close from '../icon_sox/menu_close.svg'
 
 export function Menu({ isOpen, setOpen }) {
-	if ( isOpen ) {
+	const toggleMenu = () => {
+		setOpen();
+		document.body.style.overflow = isOpen ? 'auto' : 'hidden'
+	}
 		return(
 			<> 
-		<div className="w-screen h-screen fixed top-0 left-0 z-10 flex">
-			<div className="menu" onClick={() => {
-						setOpen();
-						document.body.style.overflow = 'auto'
-						}}>
-				<img src={menu_close} alt="" className='w-10'/>
+		<div className={ "fixed top-0 left-0 z-10 flex" + 
+				( isOpen ? ' w-screen h-screen' : '' )
+				}>
+			<div className="menu" onClick={() => { toggleMenu() }}>
+				<img src={( isOpen ? menu_close : menu_open )} alt="" className='w-10'/>
 			</div>
-			<div class="w-6/12 h-screen bg-site-100 flex flex-col justify-center items-center">
+				<AnimatePresence>
+						{
+						isOpen && (
+			<motion.div
+							initial={{ width: 0, opacity:0 }}
+							animate={{ width: '50vw', opacity:1 }}
+							exit={{ width: 0, opacity: 0 }}
+							transition={{ duration: 0.3 }}
+
+							class="w-6/12 h-screen bg-site-100 flex flex-col justify-center items-center">
 				<menu className='flex flex-col justify-center gap-y-5'>
-					<Link to='/'><span className='font-code text-6xl font-bold'>Главная</span></Link>
-					<Link to='/'><span className='font-code text-6xl font-bold'>Мои работы</span></Link>
-					<Link to='/'><span className='font-code text-6xl font-bold'>Блог</span></Link>
+					<Link to='/' onClick={() => { toggleMenu() }}><span className='font-code text-6xl font-bold'>Главная</span></Link>
+					<Link to='/' onClick={() => { toggleMenu() }}><span className='font-code text-6xl font-bold'>Мои работы</span></Link>
+					<Link to='/' onClick={() => { toggleMenu() }}><span className='font-code text-6xl font-bold'>Блог</span></Link>
 					<div className='flex w-full justify-between'>
 						<img src={githubIcon} alt="" className='w-10'/>
 						<img src={vkIcon} alt="" className='w-10'/>
@@ -30,20 +42,13 @@ export function Menu({ isOpen, setOpen }) {
 						<img src={linkedInIcon} alt="" className='w-10'/>
 					</div>
 				</menu>
-			</div>
+			</motion.div>
+						)
+						}
+				</AnimatePresence>
 		</div>
 			</>
 		)
-	} else {
-		return (
-			<div className="menu" onClick={() => {
-				setOpen() 
-				document.body.style.overflow = 'hidden'
-				}}>
-				<img src={menu_open} alt="" className='w-10'/>
-			</div>
-		)
-	}
 }
 
 // TODO Надо сделать чтобы меню открывалось
