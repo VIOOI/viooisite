8f9cf303e97c30b25665385dc66d298e6b768acc
Vladimir Below <laitvladimir@yandex.ru>
2022-05-22 16:47:54 +0300

    New: Добавлена переное окружение

--- src/openDatabase.js
+++ src/openDatabase.js
@@ -1,6 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://bzsfgrkfcjubbiabbpqi.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6c2ZncmtmY2p1YmJpYWJicHFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTI5NzI4NTIsImV4cCI6MTk2ODU0ODg1Mn0.4mlbynWTUZM7i5ZK680TNFGai5ZIdCZvUdyx4whEdRk'
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_KEY)
 
