0a7fada4ef6c6de5b0ae899c3063c929568371be
Vladimir Below <laitvladimir@yandex.ru>
2022-05-24 10:59:44 +0300

    Update: Небольшие изменения в стиях ссылок главной страници

--- src/components/skils.js
+++ src/components/skils.js
@@ -1,16 +1,16 @@
 import { forwardRef } from 'react'
-import { NavLink, useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { motion } from 'framer-motion'
 
 export const Skils = forwardRef( ({ name, discription, link }, ref) => {
 	let location = useLocation()
 	return(
 		<li className="my-10" ref={ref} > 
-			<NavLink to={location.pathname == `/skils/${link}` ? '/skils' : link}>
-				<h1 className={'text-5xl font-code font-medium' + 
-					( location.pathname === `/skils/${name.toLowerCase()}` ? ' text-white' : '' )
+			<Link to={location.pathname == `/skils/${link}` ? '/skils' : link}>
+				<h1 className={'text-5xl font-code font-medium text-white' + 
+					( location.pathname === `/skils/${name.toLowerCase()}` ? ' text-blue-400' : '' )
 					}>{name}</h1>
-			</NavLink>
+			</Link>
 				<h2 className='font-code text-site-300'>{discription ?? null}</h2>
 		</li>
 	)
